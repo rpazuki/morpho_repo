05c106e1249e82ba12c1db671c4dd16b05760384
Roozbeh H. Pazuki <rpazuki@gmail.com>
2022-07-18 23:16:16 +0100

    Rewrite FitzHugh_Nagumo model

--- turing_codebase/solvers/turing_models.py
+++ turing_codebase/solvers/turing_models.py
@@ -1,16 +1,41 @@
+from idna import valid_contexto
 import numpy as np
 import numba
 
 #  $\partial_t u = D_u (\partial_x^2 + \partial_y^2)u - \alpha u + \epsilon v$
 #  $\partial_t v = D_v (\partial_x^2 + \partial_y^2)v -u + \mu v - v^3$
+# @numba.jit(nopython=True)
+# def FitzHugh_Nagumo_2(c, t, f_args):
+#     alpha, epsilon, mu = f_args
+#     u = c[0, :, :]
+#     v = c[1, :, :]
+#     fu = epsilon * (v - alpha * u)
+#     fv = -u + mu * v - v * v * v
+#     return np.stack((fu, fv))
+
+#  $\partial_t u = D_u (\partial_x^2 + \partial_y^2)u + \mu u - u^3 - v + \sigma$
+#  $\partial_t v = D_v (\partial_x^2 + \partial_y^2)v +bu - \gamma v $
 @numba.jit(nopython=True)
 def FitzHugh_Nagumo(c, t, f_args):
-    alpha, epsilon, mu = f_args
+    sigma, b, gamma, mu = f_args
     u = c[0, :, :]
     v = c[1, :, :]
-    fu = epsilon * (v - alpha * u)
-    fv = -u + mu * v - v * v * v
+    fu = mu * u - u * u * u - v + sigma
+    fv = b * u - gamma * v
     return np.stack((fu, fv))
+
+
+def FitzHugh_Nagumo_steady_state(sigma, b, gamma, mu, init_u=1.0, it=100):
+    def F(u):
+        return u * u * u - (mu - (b / gamma)) * u - sigma
+
+    def F_prime(u):
+        return 3.0 * u * u - (mu - (b / gamma))
+
+    u_star = init_u
+    for i in range(it):
+        u_star = u_star - F(u_star) / F_prime(u_star)
+    return (u_star, (b / gamma) * u_star)
 
 
 # $\partial_t u = D_u (\partial_x^2 + \partial_y^2)u + c_1 -c_0 u + c_3u^2v$
